2948490b11fc9cb621085aeac3e0f1984fea0f7f
Rufus <likeness14@gmail.com>
2024-08-01 23:26:25 +0100

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""It returns the string representation of a float."""
+
+
+def to_str(n: float) -> str:
+    """
+    It returns the string representation of a float.
+
+    Args:
+        n: A float number.
+
+    Returns:
+        str: The string representation of the float number.
+    """
+    return str(n)
